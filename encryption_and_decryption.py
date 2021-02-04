3a1f4d66fa35cd167ec45518e00d6d38f0889ddd
musenirvana <erkebulan.zhaukenov@mail.ru>
2021-02-04 23:14:15 +0600

    Use encryption and be safty:)

--- encryption_and_decryption.py
+++ encryption_and_decryption.py
@@ -1,22 +1,20 @@
 import math
 alphabit = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
-            'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+            'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', ',', '.', '!', "?", "@", "'", ':', ";", "(", ')', '-',
             'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
-            'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
-            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
-            'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+            'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', ',', '.', '!', '?', "@", "'", ':', ";", "(", ')', '-'
             ]
-probel = []
+space = []
 id_plain_text = []
 id_cypher_text = []
 id_answer = []
-word_text = input('Please write PLAIN text: ')
-cypher_text = input('Please write CYPHER TEXT: ')
+word_text = input('Write main text: ')
+cypher_text = input('Write cypher text: ')
 back_up = cypher_text
 #Encryption part 1 (Viginer part)
 for i in range(len(word_text)):
     if word_text[i] == " ":
-        probel.append(i)
+        space.append(i)
 word_text.replace(" ", '')
 
 counter = 0
@@ -25,7 +23,6 @@ for i in range(len(word_text)):
     for j in range(len(alphabit)):
         if word_text[i].lower() == alphabit[j]:
             id_plain_text.append(j)
-            print(alphabit[j])
             counter += 1
             if counter == len(word_text):
                 flag = True
@@ -39,27 +36,28 @@ for i in range(len(cypher_text)):
     for j in range(len(alphabit)):
         if cypher_text[i].lower() == alphabit[j]:
             id_cypher_text.append(j)
-            print(j)
             counter += 1
             if counter == len(cypher_text):
                 flag = True
             break
     if flag:
         break
-multiple = math.ceil(len(id_plain_text) / len(id_cypher_text))
-for i in range(multiple - 1):
+
+long_from_main = math.ceil(len(id_plain_text) / len(id_cypher_text))
+for i in range(long_from_main - 1):
     for j in range(len(id_cypher_text)):
         id_cypher_text.append(id_cypher_text[j])
 for i in range(len(id_plain_text)):
     answer = (alphabit[id_plain_text[i] + id_cypher_text[i]])
     id_answer.append(answer)
-for i in range(len(probel)):
-    id_answer.insert(probel[i], ' ')
-answer_final =''
+for i in range(len(space)):
+    id_answer.insert(space[i], ' ')
+answer_final = ''
 for i in range(len(id_answer)):
     answer_final += id_answer[i]
-print('FIRST LAYWER ANSWER: {}'.format(answer_final))
-print('FIRST LAYWER ENCRYPTION DONE!  1/2')
+print('FIRST ENCRYPTION ANSWER: {}'.format(answer_final))
+print('1/2')
+
 #Encryption part #2(Caesar part)
 number_encryption = len(cypher_text)
 plain_text_part_2 = answer_final
@@ -76,7 +74,6 @@ for i in range(len(plain_text_part_2)):
     for j in range(len(alphabit)):
         if plain_text_part_2[i] == alphabit[j]:
             id_plain_text_2.append(j)
-            print(j)
             if counter == len(plain_text_part_2):
                 flag = True
             break
@@ -90,8 +87,8 @@ for i in range(len(space_2)):
 answer_final_2 = ''
 for i in range(len(final_list_2)):
     answer_final_2 += final_list_2[i]
-print('SECOND LAYWER ANSWER: {}'.format(answer_final_2))
-print('FIRST LAYWER ENCRYPTION DONE!  2/2')
+print('SECOND ENCRYPTION ANSWER: {}'.format(answer_final_2))
+print('2/2')
 
 #Decryption part:   1/2
 print('Decryption part:   1/2')
@@ -115,7 +112,6 @@ for i in range(len(decrypted_word_list_2)):
     for j in range(len(alphabit)):
         if decrypted_word_list_2[i] == alphabit[j]:
             decrypted_id_word_list_2.append(j)
-            print(j)
             counter += 1
             if counter == len(decrypted_word_list_2):
                 flag = True
@@ -123,9 +119,8 @@ for i in range(len(decrypted_word_list_2)):
     if flag:
         break
 for i in range(len(decrypted_id_word_list_2)):
-    decrypted_temp_2 = alphabit[26 + decrypted_id_word_list_2[i] - decrypted_number_2]
+    decrypted_temp_2 = alphabit[37 + decrypted_id_word_list_2[i] - decrypted_number_2]
     decrypted_word_finaly_2.append(decrypted_temp_2)
-print(decrypted_word_finaly_2)
 
 for i in range(len(decrypted_space_location)):
     decrypted_word_finaly_2.insert(decrypted_space_location[i], ' ')
@@ -133,10 +128,10 @@ for i in range(len(decrypted_space_location)):
 decrypted_finaly_answer_2 = ''
 for i in range(len(decrypted_word_finaly_2)):
     decrypted_finaly_answer_2 += decrypted_word_finaly_2[i]
-print(decrypted_finaly_answer_2)
+print('Answer: {}'.format(decrypted_finaly_answer_2))
+
 #Decryption part:   2/2
 print('Decryption part:   2/2')
-
 decrypted_word_3 = decrypted_finaly_answer_2
 decrypted_cypher_3 = cypher_text
 decrypted_cypher_list_3 = []
@@ -145,7 +140,6 @@ decrypted_space_location_3 = []
 decrypted_id_cypher_list_3 = []
 decrypted_id_word_list_3 = []
 decrypted_word_finaly_3 = []
-
 
 for i in range(len(decrypted_word_3)):
     if decrypted_word_3[i] == ' ':
@@ -181,17 +175,15 @@ for i in range(len(decrypted_cypher_list_3)):
     if flag:
         break
 for i in range(len(decrypted_id_word_list_3)):
-    decrypted_temp_3 = alphabit[26 + decrypted_id_word_list_3[i] - decrypted_id_cypher_list_3[i]]
+    decrypted_temp_3 = alphabit[37 + decrypted_id_word_list_3[i] - decrypted_id_cypher_list_3[i]]
     decrypted_word_finaly_3.append(decrypted_temp_3)
-print(decrypted_word_finaly_3)
 
 for i in range(len(decrypted_space_location_3)):
     decrypted_word_finaly_3.insert(decrypted_space_location_3[i], ' ')
 
-decrypted_finaly_answer_3 =''
+decrypted_finaly_answer_3 = ''
 for i in range(len(decrypted_word_finaly_3)):
     decrypted_finaly_answer_3 += decrypted_word_finaly_3[i]
 
-print(decrypted_finaly_answer_3)
-print(decrypted_space_location_3)
+print('Answer: {}'.format(decrypted_finaly_answer_3))
 print('FINALY DONE!  2/2')
